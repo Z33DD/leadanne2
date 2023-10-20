b82f4f12ea2728f3e3bef989f24d53a21970a0b4
Pedro Antonio <pedro@metrobi.com>
2023-10-20 09:12:08 -0300

    Add ChatGPT integration

---
+++ core/chatgpt.py
@@ -0,0 +1,26 @@
+import openai
+
+from leadanne2.settings import OPENAI_KEY
+
+openai.api_key = OPENAI_KEY
+
+completion = openai.ChatCompletion()
+
+
+def askgpt(question, chat_log=None):
+    if chat_log is None:
+        chat_log = [
+            {
+                "role": "system",
+                "content": "You are a helpful, upbeat and funny assistant.",
+            }
+        ]
+    chat_log.append({"role": "user", "content": question})
+    response = completion.create(model="gpt-3.5-turbo", messages=chat_log)
+    answer = response.choices[0]["message"]["content"]
+    chat_log.append({"role": "assistant", "content": answer})
+    return answer, chat_log
+
+
+if "__main__" in __name__:
+    print(askgpt("How do I write a hello world in python?"))
--- leadanne2/settings.py
+++ leadanne2/settings.py
@@ -9,8 +9,12 @@ https://docs.djangoproject.com/en/4.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
-
+import os
 from pathlib import Path
+from dotenv import load_dotenv
+
+
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -127,3 +131,5 @@ STATIC_URL = "static/"
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+OPENAI_KEY = os.environ.get("OPENAI_KEY")
