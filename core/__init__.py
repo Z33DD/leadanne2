546ff7daa896b16e88a78105e4e27cdae2108836
Pedro Antonio <pedro@metrobi.com>
2023-12-14 09:40:20 -0300

    Migrate to serverless architecture

--- core/__init__.py
+++
@@ -1,5 +0,0 @@
-from postmarker.core import PostmarkClient
-
-from leadanne2.settings import POSTMARK_SERVER_TOKEN
-
-postmark = PostmarkClient(server_token=POSTMARK_SERVER_TOKEN)
--- tests.py
+++
@@ -1,9 +0,0 @@
-from core.tasks import generate_result
-import json
-
-with open("webhook_payload_pt.json", "r") as fp:
-    webhook_payload = json.load(fp)
-
-rst = generate_result(webhook_payload)
-
-print(rst)
